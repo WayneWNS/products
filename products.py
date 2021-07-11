eecb2f6078cef4c94714e3d102628358eb13fddc
Wayne Su <runewns@gmail.com>
2021-07-11 21:11:27 +0800

    add header

--- products.py
+++ products.py
@@ -8,10 +8,12 @@ while True:
 	products.append([name, price]) # 簡化版2
 
 print(products)
-products[0][0]
-
 
 for p in products:
-	print(p) # 印出每一筆小清單
-	print(p[0]) # 印出每個清單的第一個值，在這裡就是商品名稱
 	print(p[0], '的價格是', p[1]) # 將小清單內的數值都印出來，並附上說明
+
+# 寫入檔案時加入encoding='utf-8'來解決中文亂碼問題
+with open('products.csv', 'w', encoding = 'utf-8') as f: # 檔案也可存成txt/csv檔，csv較常用
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n') # f.write 才是真正的寫入
